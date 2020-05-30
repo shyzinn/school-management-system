304d709f18793aba71b2bc8f65b04d230f9a7a61
shyzinn <44265970+shyzinn@users.noreply.github.com>
2020-05-30 13:25:57 +0100

    Initial commit

---
+++ src/db/CreateTables.java
@@ -0,0 +1,32 @@
+package db;
+// Gustavo Henrique de Souza - 20052 \\
+// OOP - Dorset College \\
+
+public class CreateTables {
+
+    CreateDatabase table;
+
+    public CreateTables() {
+
+        table = new CreateDatabase();
+
+        table.createDb();
+        table.setAdminCredential();
+        table.setCourses();
+        table.setTimeTable();
+        table.setFaculty();
+        table.setStudents();
+        table.setAssignments();
+        table.setUploadedAssignments();
+        table.setResults();
+        table.setAttendance();
+        table.setFacultyPivotCourses();
+        table.setAttendancePivotCourses();
+        table.setStudentPivotCourses();
+        table.setExams();
+        table.setAssignmentsResults();
+        table.setUploadedExams();
+        table.setExamsResults();
+        table.setCoursePlan();
+    }
+}
